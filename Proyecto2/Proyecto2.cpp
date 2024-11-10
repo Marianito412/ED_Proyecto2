603d1eda2010342722a13cefe1960130061ece05
Steven <s.loaiciga.1@estudiantec.cr>
2024-11-10 17:29:07 -0600

    Update Proyecto2.cpp

--- Proyecto2/Proyecto2.cpp
+++ Proyecto2/Proyecto2.cpp
@@ -218,7 +218,7 @@ void SelectKeyGen(OpGenerator* Generator)
 
     if (Resultado == "Ascendente")
     {
-        Generator->KeyGenerator = nullptr;
+        Generator->KeyGenerator = new AscendingKeyGenerator();
     }
     else if (Resultado == "Aleatorio")
     {
