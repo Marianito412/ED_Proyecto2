f2c0c8c8b7e47b193f2362218c7d781bae68b9f4
Steven <s.loaiciga.1@estudiantec.cr>
2024-11-10 17:20:57 -0600

    Create AscendingKeyGenerator.h

---
+++ Proyecto2/ClasesProyecto/AscendingKeyGenerator.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "OpPartGenerator.h"
+
+class AscendingKeyGenerator : public OpPartGenerator
+{
+public:
+    int i = 1;
+    void Reset() {
+        i = 1;
+    }
+	void Next(Operation* Op, Dictionary<int, int>* Dict) {
+
+        if (Op->Op == OpType::Insert || Op->Op == OpType::Find)
+        {
+            Op->Key = i;
+            i++;
+        }
+
+            if (Op->Op == OpType::Remove)
+            {
+                while (Dict->contains(i)) {
+                    Dict->remove(i);
+                }
+                i++;
+            }
+		}
+	
+};
