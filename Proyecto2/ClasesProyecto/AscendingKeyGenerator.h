78d066dd0782d99bca0d26516478672bf1682d70
Steven <s.loaiciga.1@estudiantec.cr>
2024-11-12 01:18:43 -0600

    Update AscendingKeyGenerator.h

--- Proyecto2/ClasesProyecto/AscendingKeyGenerator.h
+++ Proyecto2/ClasesProyecto/AscendingKeyGenerator.h
@@ -1,6 +1,12 @@
-#pragma once
+﻿#pragma once
 #include "OpPartGenerator.h"
-
+/*
+    Autor: Steven David Loaíciga Segura
+    Recibe: Operation Type, que es una enumeraci�n de las distintas opciones que se pueden realizar en el diccionario. Y el diccionario.
+    Funcion: Tiene 1 atributo:
+        1- i: es el número de iteraciones de inserciones y borrados.
+    Esto va a generar una key válida según el caso en el que se usen Keys por Ascendencia.
+*/
 class AscendingKeyGenerator : public OpPartGenerator
 {
 public:
